ab82813e873e840f99868e72d072d573b07930b3
Amit Prakash <amitprakash0111@gmail.com>
2021-10-30 19:25:31 +0530

    till Imperative React

---
+++ hacker-stories/src/App.js
@@ -0,0 +1,136 @@
+import * as React from "react";
+
+const welcome = {
+  greeting: "Hey",
+  title: "React",
+};
+
+function getTitle() {
+  return welcome.title;
+}
+
+//custom react hook
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+};
+
+const App = () => {
+  console.log(" App renders ");
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React");
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  React.useEffect(() => {
+    localStorage.setItem("search", searchTerm);
+  }, [searchTerm]);
+
+  const searchedStories = stories.filter((story) => {
+    return story.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
+  return (
+    <div>
+      <h1>{welcome.greeting + " " + getTitle()}</h1>
+
+      <InputWithLabel
+        id="search"
+        label="Search"
+        value={searchTerm}
+        isFocused
+        onInputChange={handleSearch}
+      >
+        <strong>Search:</strong>
+      </InputWithLabel>
+      <hr />
+
+      {/* render the list */}
+      <List list={searchedStories} />
+    </div>
+  );
+};
+
+const List = ({ list }) => {
+  console.log(" List renders ");
+  return (
+    <ul>
+      {list.map(({ objectID, ...item }) => (
+        <Item key={objectID} {...item} />
+      ))}
+    </ul>
+  );
+};
+
+const Item = ({ url, title, author, num_comments, points }) => (
+  <li>
+    <span>
+      <a href={url}>{title}</a>
+    </span>
+    <span>{author}</span>
+    <span>{num_comments}</span>
+    <span>{points}</span>
+  </li>
+);
+
+const InputWithLabel = ({
+  id,
+  value,
+  type = "text",
+  onInputChange,
+  isFocused,
+  children,
+}) => {
+  console.log(" Search renders ");
+
+  const inputRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+
+  return (
+    <>
+      <label htmlFor={id}>{children}</label>
+      &nbsp;
+      <input
+        ref={inputRef}
+        id={id}
+        type={type}
+        value={value}
+        autoFocus={isFocused}
+        onChange={onInputChange}
+      />
+    </>
+  );
+};
+
+export default App;
